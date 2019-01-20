794f69c6f7eaf30ed981e0705d32b61405d50b95
brandonmandzik <35039517+brandonmandzik@users.noreply.github.com>
2019-01-20 22:42:25 +0100

    update cube frag

--- aufgabe3_f.glsl
+++ aufgabe3_f.glsl
@@ -3,12 +3,18 @@
 uniform mat4 modelMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
+uniform sampler2D textureSampler;
+
 out vec3 color;
+
+in vec3 FragPos;
 in vec3 eckenFarben;
 in vec3 normalVector;
-//uniform vec3 lightPos = vec3(2, 4, 1.5f);  
+in vec2 uvs;
+
+ 
 uniform vec3 lightPos = vec3(200, 100, 100); 
-in vec3 FragPos; 
+vec3 lightColor = vec3(0.8f, 0.8f, 0.8f);
 uniform vec3 viewPos= vec3 (0,0,0);
 
 
@@ -17,10 +23,11 @@ uniform vec3 viewPos= vec3 (0,0,0);
 
 
 void main(){
+
  	float specularStrength = 0.5f;
 	
-	float ambientStrength = 0.3;
-    vec3 ambient = ambientStrength * eckenFarben;
+	float ambientStrength = 0.3f;
+    vec3 ambient = ambientStrength * lightColor;
 	
 
 	vec3 tempnormal = normalize(normalVector);
@@ -29,19 +36,20 @@ void main(){
 	
 	
 	float diff = max(dot(tempnormal, lightDir), 0.0);
-	vec3 diffuse = diff * eckenFarben;
+	vec3 diffuse = diff * lightColor;
 	
 	vec3 viewDir = normalize(viewPos - FragPos);
 
 	vec3 reflectDir = reflect(-lightDir, tempnormal);
 
 	
-	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-	vec3 specular = specularStrength * spec * vec3(1.0,1.0,1.0); 
+	float spec = pow(max(dot(viewDir, reflectDir), 0.0), 8);
+	vec3 specular = specularStrength * spec * lightColor; 
 	
-
-	vec3 result = ambient + diffuse+  specular;
-	color = result;
+	vec4 texel = texture(textureSampler, uvs);
+	
+	vec3 result = (ambient + diffuse +  specular) ;
+	color = texel.xyz*result ;
 	
 }
 
